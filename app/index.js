0df100d3859de930ed12b4fb27a518f0eca31f49
Saptarshi Basu <sapb2004@gmail.com>
2019-07-13 12:38:47 +0530

    initial version

---
+++ app/index.js
@@ -0,0 +1,27 @@
+require('express-async-errors');
+const winston = require('winston');
+const express = require('express');
+const config = require('config');
+const http = require('http');
+const compression = require('compression');
+const helmet = require('helmet');
+const posts = require('./routes/posts');
+const error = require('./middleware/error');
+
+const app = express();
+winston.configure({transports: [new winston.transports.File({ filename: 'logfile.log' }) ]});
+app.use(helmet());
+app.use(express.json());
+app.use(compression());
+
+app.use(express.static('public'));
+
+app.use('/api/posts', posts);
+
+app.use(error);
+
+const port = process.env.APP_PORT || 4000;
+const appName = config.get('app_name');
+
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => console.log(`${appName} Listening on port ${port}...`));
